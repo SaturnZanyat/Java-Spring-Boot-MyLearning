ddea21d821cca4aedabbbb8a15fa3d61ed420c5b
Патрикеев Денис Николаевич <s48554@psta.ru>
2023-03-22 14:56:10 +0300

    init

---
+++ src/main/java/com/demo/Controller.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String Home(){
+        return "HOME";
+    }
+}
